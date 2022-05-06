8bace1ff30e4eef243560641f04bda6a46d0d4b8
v_kilzhang <v_kilzhang@tencent.com>
2022-05-06 18:28:55 +0800

    增加robot组件

---
+++ src/components/Robot.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface RobotProps {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const Robot: React.FC<RobotProps> = (props) => {
+  return <></>;
+};
+
+export default Robot;
