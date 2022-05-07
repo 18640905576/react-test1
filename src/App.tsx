0cd5ae30bc4ddde2e34a8106ae51432f45e94800
v_kilzhang <v_kilzhang@tencent.com>
2022-05-07 17:16:02 +0800

    feat: 增加css模块化功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
+import styles from "./App.module.css";
 import robots from "./mockdata/robots.json";
 import Robot from "./components/Robot";
 
 function App() {
   return (
-    <ul>
-      {robots.map((r) => (
-        <Robot id='r.id' name='r.name' email='r.email' />
-      ))}
-    </ul>
+    <div>
+      <ul className={styles.robotList}>
+        {robots.map((r) => (
+          <Robot id={r.id} name={r.name} email={r.email} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import styles from "./Robot.module.css";
 
 interface RobotProps {
   id: string;
   name: string;
   email: string;
 }
+// // 用户的输入
+// const userProvidePropsString = `{"dangerouslySetInnerHTML":{"__html":"<img onerror='alert(\\"xss\\");' src='empty.png' />"}}`;
+// // 经过 JSON 转换
+// const userProvideProps = JSON.parse(userProvidePropsString);
 
 const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
+  // return <div {...userProvideProps} />;
+
   return (
-    <li>
+    <li className={styles.cardContainer}>
       <img src={`https://robohash.org/${id}`} alt='' />
       <h2>{name}</h2>
       <p>{email}</p>
